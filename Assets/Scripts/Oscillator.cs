9ee42e90f85ea5c25348ea4c95c84db5462a5f95
Ian LS <iansouza.sjc@gmail.com>
2022-12-21 06:35:31 -0300

    Add oscillator to obstacles

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 2f;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) { return; } // Mathf.Epsilon instead of 0
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so it's cleaner
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
