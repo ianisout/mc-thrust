e6c28aab2724692b71f6d935de2c5e5409daddee
Ian LS <iansouza.sjc@gmail.com>
2022-12-27 06:39:33 -0300

    audio singleton for menu scenes

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class AudioPlayer : MonoBehaviour
 {
-    // [SerializeField] AudioClip audioClip;
     static AudioPlayer instance;
     AudioSource audioSource;
     string scene;
@@ -14,57 +13,29 @@ public class AudioPlayer : MonoBehaviour
     }
 
     void Awake() {
-        // ManageSingleton();
-    }
-
-    void Start()
-    {
-        // Debug.Log("Start: " + SceneManager.GetActiveScene().name);
-        // audioSource = instance.GetComponentInChildren<AudioSource>();
-        // audioClip = GetComponent<AudioClip>();
-        // Debug.Log("audioSource: " + audioSource);
-        // Debug.Log(audioClip.name);
+        scene = SceneManager.GetActiveScene().name;
+        ManageSingleton();
     }
 
     void ManageSingleton()
     {
-        // Debug.Log("Start: " + SceneManager.GetActiveScene().name);
-
-        // Debug.Log(instance);
-        // if (SceneManager.GetActiveScene().name == "Over")
-        // {
-        //     Debug.Log(instance);
-        // }
-        // {
-            // instance.GetComponentInChildren<AudioSource>().enabled = false;
-        // }
-        // else if (SceneManager.GetActiveScene().name == "GameOver")
-        /* {
+        if (scene == "Space1" || scene == "Office" || scene == "Jungle")
+        {
+            instance.GetComponentInChildren<AudioSource>().enabled = false;
+        }
+        else if (instance != null && SceneManager.GetActiveScene().name == "MainMenu")
+        {
             instance.GetComponentInChildren<AudioSource>().enabled = true;
         }
-        else */
-        
-        if (instance != null && SceneManager.GetActiveScene().name == instance.GetComponentInChildren<AudioSource>().clip.name)
+        else if (instance != null)
         {
             gameObject.SetActive(false);
             Destroy(gameObject);
         }
         else
         {
-            // if (instance != null && SceneManager.GetActiveScene().name != instance.GetComponentInChildren<AudioSource>().clip.name)
-            // {
-            //     Destroy(gameObject);
-            // }
-
             instance = this;
             DontDestroyOnLoad(gameObject);
-
-            if (SceneManager.GetActiveScene().name != null && 
-                instance.GetComponentInChildren<AudioSource>().clip.name != null &&
-                SceneManager.GetActiveScene().name != instance.GetComponentInChildren<AudioSource>().clip.name)
-            {
-                Destroy(gameObject);
-            }
         }
     }
 }
